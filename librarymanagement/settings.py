e84cb12a57a0a6ffd773198e73b69b3617690ad6
Nelson Machado <2100607@ALUNO.UNIVESP.BR>
2026-04-05 17:16:25 -0300

    Alteração email

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -127,7 +127,7 @@ STATIC_DIR,
 LOGIN_REDIRECT_URL='/afterlogin'
 
 #for email
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
